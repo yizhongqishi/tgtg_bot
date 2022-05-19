f0f51365ccba16d148c96ddd34cb3bc15c44be83
yizhongqishi <yizhongqishi@163.com>
2022-05-19 15:55:25 +0100

    update something

--- bot.py
+++ bot.py
@@ -10,13 +10,17 @@ from tgtg import TgtgClient
 import time
 import random
 import datetime
+
+
 send = False
+sleep_from = 1917
+sleep_to = 900
+item_id = 600644
 
 
 def start(update: Update, context: CallbackContext):
-    global state
     context.bot.send_message(chat_id=update.effective_chat.id, text="Welcome! My current state is {}".format(state))
-    logging.info("this user {}".format(update.effective_chat.id))
+    logging.info("this user is {}".format(update.effective_chat.id))
 
 
 def unknown(update: Update, context: CallbackContext):
@@ -25,26 +29,28 @@ def unknown(update: Update, context: CallbackContext):
 
 def add_notion(update: Update, context: CallbackContext):
     context.bot.send_message(chat_id=update.effective_chat.id, text="OKOK")
-    logging.info("New ID:"+str(update.effective_chat.id))
+    logging.info("Please Add New ID:"+str(update.effective_chat.id))
 
 
-def send_notion():
-    with open("id.list", "r") as f:
+def send_notion(mes="New Items!", user="common"):
+    with open(user+'.list', "r") as f:
         ids = f.readlines()
         for id in ids:
-            bot.send_message(chat_id=int(id), text="New Items!")
+            bot.send_message(chat_id=int(id), text=mes)
 
 
 def send_alert(mes="Something Wrong!"):
-    with open("admin.id", "r") as f:
-        admin_id = f.readline()
-        bot.send_message(chat_id=int(admin_id), text=mes)
+    send_notion(mes, "admin")
 
 
 def check_again(update: Update, context: CallbackContext):
     global send
     send = False
-    context.bot.send_message(chat_id=update.effective_chat.id, text="OK, will watch that for you")
+    now = daytime()
+    if now >= sleep_from or now <= sleep_to:
+        context.bot.send_message(chat_id=update.effective_chat.id, text="Out of business hours!")
+    else:
+        context.bot.send_message(chat_id=update.effective_chat.id, text="OK, will watch that for you")
 
 
 def daytime():
@@ -54,8 +60,6 @@ def daytime():
 
 
 def tg_client():
-    sleep_from = 1915
-    sleep_to = 1200
     global send
     global state
     with open("tg.token", "r") as f:
@@ -65,30 +69,26 @@ def tg_client():
     while True:
         now = daytime()
         if now >= sleep_from or now <= sleep_to or send:
-            if now >= sleep_from or now <= sleep_to:
-                send = False
-            logging.info("Sleeping Time")
-            send_alert("I'm Sleeping")
-            state = "Sleeping"
-            while now >= sleep_from or now <= sleep_to or send:
-                logging.info("Current time is {}, still sleeping".format(now))
-                time.sleep(60)
-                now = daytime()
-            logging.info("Current time is {}, time to wake up".format(now))
-            send_alert("Wake up now")
-            state = "Watching"
-        try:
-            item = client.get_item(600644)
-            if item['items_available'] > 0:
-                logging.info("New things, hurry up!")
-                send_notion()
-                send = True
-            else:
-                time.sleep(random.randrange(50, 60))
-        except Exception as e:
-            logging.error(e)
-            send_alert()
-            break
+            send = (sleep_from > now > sleep_to)
+            logging.info("Current time is {}, still sleeping".format(now))
+            time.sleep(60)
+        else:
+            try:
+                send_notion("Wake up now")
+                state = "Watching"
+                item = client.get_item(item_id)
+                if item['items_available'] > 0:
+                    logging.info("New things, hurry up!")
+                    send_notion()
+                    state = "Sleeping"
+                    send_notion("I'm Sleeping")
+                    send = True
+                else:
+                    time.sleep(random.randrange(20, 30))
+            except Exception as e:
+                logging.error(e)
+                send_alert()
+                break
 
 
 if __name__ == "__main__":
